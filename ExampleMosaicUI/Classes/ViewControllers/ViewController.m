768bfb928717eb8ca7b5a16c812889de22c89408
Ezequiel Alejandro Becerra <ezequiel.becerra@gmail.com>
2013-01-13 19:31:44 -0300

    Added snapshots on custom viewcontroller to make rotations smoother

--- ExampleMosaicUI/Classes/ViewControllers/ViewController.m
+++ ExampleMosaicUI/Classes/ViewControllers/ViewController.m
@@ -13,6 +13,24 @@
 
 @implementation ViewController
 
+#pragma mark - Private
+
+static UIImageView *captureSnapshotOfView(UIView *targetView){
+    UIImageView *retVal = nil;
+    
+    UIGraphicsBeginImageContextWithOptions(targetView.bounds.size, YES, 0);
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    [[targetView layer] renderInContext:currentContext];
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+        
+    retVal = [[UIImageView alloc] initWithImage:image];
+    retVal.frame = [targetView frame];
+    
+    return retVal;
+}
+
 #pragma mark - Public
 
 - (void)viewDidLayoutSubviews{
@@ -27,6 +45,28 @@
 - (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
+    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    snapshotBeforeRotation = captureSnapshotOfView(mosaicView);
+    [self.view insertSubview:snapshotBeforeRotation aboveSubview:mosaicView];
+}
+
+-(void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
+    snapshotAfterRotation = captureSnapshotOfView(mosaicView);
+    
+    snapshotBeforeRotation.alpha = 0.0;
+    [self.view insertSubview:snapshotAfterRotation belowSubview:snapshotBeforeRotation];
+    mosaicView.hidden = YES;
+}
+
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation{
+    [snapshotBeforeRotation removeFromSuperview];
+    [snapshotAfterRotation removeFromSuperview];
+    snapshotBeforeRotation = nil;
+    snapshotAfterRotation = nil;
+    mosaicView.hidden = NO;
 }
 
 #pragma mark - MosaicViewDelegateProtocol
